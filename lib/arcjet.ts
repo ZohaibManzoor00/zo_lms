8f61ea5e26cec372c46942e528b9f2f16f3a6976
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-06-18 22:08:20 -0400

    arcjet integrated

---
+++ lib/arcjet.ts
@@ -0,0 +1,27 @@
+import arcjet, {
+  fixedWindow,
+  protectSignup,
+  slidingWindow,
+  shield,
+  sensitiveInfo,
+} from "@arcjet/next";
+import { env } from "./env";
+
+export {
+  arcjet,
+  fixedWindow,
+  protectSignup,
+  slidingWindow,
+  shield,
+  sensitiveInfo,
+};
+
+export default arcjet({
+  key: env.ARCJET_KEY,
+  characteristics: ["fingerprint"],
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+  ],
+});
